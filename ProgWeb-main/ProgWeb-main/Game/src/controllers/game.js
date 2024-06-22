39883da23801ae17c907ea61474dfb13ed11d74d
Matheus de Oliveira <123041784+matheuscooper@users.noreply.github.com>
2024-06-22 19:02:44 -0300

    Delete ProgWeb-main/ProgWeb-main directory

--- ProgWeb-main/ProgWeb-main/Game/src/controllers/game.js
+++
@@ -1,7 +0,0 @@
-
-const game = (req, res) => {
-    // const conteudo = 'Página sobre a aplicação';
-    // res.render('game/index', {layout: false});};
-    res.render('game/index');};
-
-module.exports = { game}
--- ProgWeb-main/ProgWeb-main/JS1/script.js
+++
@@ -1,8 +0,0 @@
-
-for (let i = 1 ; i < 11 ; i++){
-    document.write("<table><thead><tr><th colspan="+2+">Produtos de " + i + "</th></tr></thead><tbody>");
-    for (let j = 1 ; j < 11 ; j++){
-        document.write("<tr><td>" + i + "X" + j + "</td><td>" + i*j + "</td>");
-    }
-    document.write("</table>");
-}
